01d56512c5ea148bb3b580fd6d1b4da7e980bf2b
Parnell <psteide@ford.com>
2022-03-31 14:57:47 -0400

    Started

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function logValues(){
-  // Your code here
-}
+
+const button = document.querySelector("button")
+
+
+button.addEventListener("click",
+
+  function logValues() {
+    const inputs = document.querySelectorAll("input").value
+    inputs.forEach(input => console.log(input))
+  }
+)
